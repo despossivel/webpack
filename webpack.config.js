6161695ae923ce93d5f15c9c7494a92ba3f5f3c6
matheus <matheus@ti>
2018-11-17 10:09:35 -0300

    configurando webpack basico

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const path = require('path');
+
+module.exports = {
+
+	entry:path.join(__dirname, 'src', 'index'),
+	output:{
+		path:path.join(__dirname,'dist'),
+		filename: 'bundle.js'
+	}
+
+};
+
