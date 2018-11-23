b9f25f6e2e2ea4d7994337bd16cce837de110fa6
matheus <matheus@ti>
2018-11-23 11:57:38 -0300

    Usando Babel

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var React = require('react'); 
+var createReactClass = require('create-react-class');
 
-var Title = React.createClass({
+console.log('ye');
+
+var Title = createReactClass({
 	render:function(){
 		return React.createElement('h1',null, 'Titulo');
 	}
 });
+
+
+//ReactDOM.render(<App />, document.getElementById('app'));
 
 module.exports = Title;
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,27 @@ module.exports = {
 		path:path.join(__dirname,'dist'),
 		filename: 'bundle.js',
 		publicPath:'/dist/'
-	}
+	},
+	
+	// module:{
+	// 	loaders:[{
+	// 		test: /\.js?$/,
+	// 		exclude: /node_modules/,
+	// 		include: /src/,
+	// 		loader: 'babel-loader',
+	// 	}]
+	// }
+	module: {
+		rules: [{
+		  test: /\.js$/,
+		  exclude: /node_modules/,
+		  include: /src/,
+		  loader: 'babel-loader',
+		  query: {
+			presets: ['es2015']
+		  }
+		}]
+	  }
 
 };
 
