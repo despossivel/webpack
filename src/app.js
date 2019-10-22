7f24731b8e794afcc95311b9b2ac8f92108bc074
despossivel <mattbmoller@gmail.com>
2019-10-21 22:28:14 -0300

    simple config

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 'use strict';
 
-import React from 'react'; 
+import React from 'react';
 import createReactClass from 'create-react-class';
- 
 
 const App = createReactClass({
-	render:function(){
-		return <h1>Aplicacao </h1>;
-	}
+	render: () => <h1>Aplicacao </h1>
 });
-
-
-//ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,30 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App  from './app';
+import App from './app';
 
-const renderApp =(NextApp) =>{
-    ReactDOM.render(
-        <AppContainer>
-           <NextApp />
-        </AppContainer>,document.querySelector('[data-js="app"]'));
-}
-
+const renderApp = (NextApp) => ReactDOM.render(<AppContainer>
+    <NextApp />
+</AppContainer>, document.querySelector('[data-js="app"]'));
 
 renderApp(App);
 
-//  ReactDOM.render(
-//  <AppContainer>
-//     <App />
-//  </AppContainer>,document.querySelector('[data-js="app"]'));
-
-
-
- if(module.hot){
-     module.hot.accept('./app',()=>{
+if (module.hot) {
+    module.hot.accept('./app', () => {
         const NextApp = require('./app').default;
-
         renderApp(NextApp);
-
-     });
- }
+    });
+}
